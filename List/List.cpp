8f1438e75e05520ce518d8c284f44009e256e78a
陈宏皓 <17278910788@163.com>
2026-05-08 14:16:18 +0800

    Add files via upload

---
+++ List/List.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "List.h"
+#include <list>
+#include <initializer_list>
+using namespace std;
+int main()
+{
+	initializer_list<int> ilt = { 1,2,3,4,5 };
+	return 0;
+}
